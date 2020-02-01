34da96e18dda5baf4c7eec936fc4ef2c9ec244a3
Cláudia Langer <langer.langer72@gmail.com>
2020-02-01 10:31:55 -0300

    'ResponsiveLayout'

--- js/validate.js
+++ js/validate.js
@@ -1,31 +1,45 @@
-//Menu Sidebar
-$(window).resize(function() {
+//loader da logo para entrar 30s depois de carregar a página
+//setTimeout(function(){
+  //  document.getElementById('logo-load').classList.remove('hide');
+   //}, 5000);
+/*$(function(){
     var tamanhoJanela = $(window).width();
-    if (tamanhoJanela < 800) { 
-        $(".nav-link-img").css("left", "5%");
-        $(".nav-link-img").css("top", "99.5%");
-        $(".nav-link-img1").css("left", "15%");
-        $(".nav-link-img1").css("top", "100%");
-        $(".intro-img-local").remove();
-        $(".intro-img-local1").remove();
-        $(".intro-img-whats").remove();
-        $(".intro-img-fone").remove();
-        $(".nav-link-img-2").remove();
-        $(".nav-link-img-3").remove();
-    } else {
-        $(".nav-link-img").css("left", "90%");
-        $(".nav-link-img").css("top", "30%");
-        $(".nav-link-img1").css("left", "95%");
-        $(".nav-link-img1").css("top", "30%");
+    alert(tamanhoJanela);
+    if (tamanhoJanela > 1025) {   
+        $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.nav-ocult').css("position", "fixed");
+            $('.nav-ocult').css("display", "block");
+            $('.nav-ocult').css("width", "100%");
+        } else {
+            $('.nav-ocult').css("display", "none");
+        }
+        });
     }
+});*/
+
+
+//mostra e ocultar icones de rede social, não ta funcionando para ipad, ver
+$ (function () {
+    var contEvent = 0;
+    $(".navbar-toggler").click( function() {
+        if($(".navbar-taggler").on()){
+            contEvent = contEvent + 1;
+            if(contEvent == 3){
+                contEvent = 1;
+            }if(contEvent == 1){
+                $('.display-control').css("display", "none");
+            }if(contEvent == 2){
+                $('.display-control').css("display", "block");
+            }
+        }      
+    });
 });
 
 $(function(){
     var tamanhoJanela = $(window).width();
     if (tamanhoJanela >= 768 && tamanhoJanela <= 1024) {
         $(".remove").remove();
-        $('.nav-link-img').remove();
-        $(".nav-link-img1").remove();
     }
 });;
 
