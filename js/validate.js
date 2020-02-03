c02ed819b809d79d6bcb2ddc1dcacda96119b8f2
Cláudia Langer <langer.langer72@gmail.com>
2020-02-03 18:49:07 -0300

    'Anima-de-menu'

--- js/validate.js
+++ js/validate.js
@@ -2,22 +2,37 @@
 //setTimeout(function(){
   //  document.getElementById('logo-load').classList.remove('hide');
    //}, 5000);
-/*$(function(){
+
+$(function(){
     var tamanhoJanela = $(window).width();
-    alert(tamanhoJanela);
     if (tamanhoJanela > 1025) {   
-        $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('.nav-ocult').css("position", "fixed");
-            $('.nav-ocult').css("display", "block");
-            $('.nav-ocult').css("width", "100%");
-        } else {
+        $(document).ready(function(){
+            $(window).scroll(function () {
+                var div_heigh = $(".clouse-menu").height(); // pega a altura da div
+                var win_heigh = window.innerHeight; // pega a altura da janela
+                var win_scrol = $(this).scrollTop(); // pega o valor da rolagem da janela
+                var div_topo  = $(".clouse-menu").offset().top; // distância da div até o início do documento
+                var distancia = div_topo - win_scrol - win_heigh; // distância da div até a borda inferior da janela
+                if (win_scrol > 50) {
+                    $('.nav-ocult').css("position", "fixed");
+                    $('.nav-ocult').css("display", "block");
+                    $('.nav-ocult').css("backgroundImage", "linear-gradient(130deg, #fe00005d, #e6a856d5 89%)");
+                    $('.nav-ocult').css("width", "100%");
+                    if(distancia <= -div_heigh){
+                        $('.nav-ocult').css("display", "none");
+                    }
+                }
+                else {
+                    $('.nav-ocult').css("display", "none");
+                }
+            });
             $('.nav-ocult').css("display", "none");
-        }
         });
     }
-});*/
-
+    else{
+        $('.nav-ocult').remove().addClass('.display-control');
+    }
+});
 
 //mostra e ocultar icones de rede social, não ta funcionando para ipad, ver
 $ (function () {
@@ -58,6 +73,3 @@ $(function(){
         $(".photo1").css("width", "96%");
     })
 });
-
-
-
